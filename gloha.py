8d690e0426358e4bceeb55c27c97627d39dbe32f
asuka <asuka>
2023-02-27 22:02:49 +0900

    Single process

--- gloha.py
+++ gloha.py
@@ -4,13 +4,14 @@ import subprocess
 import time
 import multiprocessing
 import json
-from os import system
+import os
 
 import azlib.ut as aut
 import azlib.pr as apr
 import azlib.json as ajs
 import tcp_latency
 import ping_latency
+import scheduler
 
 FNAME_CONFIG_UPDATED = './FLAG_CONFIG_UPDATED'
 PATH_PID = '/tmp/gloha_pid.json'
@@ -30,6 +31,14 @@ class GHA_INSTANCE:
 		self.status_list = {}
 		self.pid = -99
 		self._serverUnpack()
+
+		self.nodeSelected = '0'
+		self.isNodeSelected = False
+		self.isNodeSwitching = True
+		self.taskproc = None
+
+	def getInterval(self):
+		return self.check_interval
 
 	def _wait4Lock(self):
 		global lock_0
@@ -66,84 +75,73 @@ class GHA_INSTANCE:
 		return self.label
 
 	def exec(self):
-		nodeSelected = '0'
-		isNodeSelected = False
-		isNodeSwitching = True
-		taskproc = None
-
 		if self.select_scheme == 'priority':
 			## Until 2023.02.25, "priority" is the only scheme of GLOHA
-			while True:
-				self.log.print('Checking if node-0 available...')
-				la_0 = self.getLatency(self.server_list['0']['host'], self.check_scheme, timeout=self.server_list['0']['timeout']/1000.)
-				
-				## When node-0 failed, select an available one.
-				## --- Node failover logic
-				if la_0 < 0:
-					self.log.print('Node-0 seems not available. Check the next.')
-					n_nodes = len(self.server_list.keys())
 
+			self.log.print('[%s] Checking if node-0 available...' % self.label)
+			la_0 = self.getLatency(self.server_list['0']['host'], self.check_scheme, timeout=self.server_list['0']['timeout']/1000.)
+			
+			## When node-0 failed, select an available one.
+			## --- Node failover logic
+			if la_0 < 0:
+				self.log.print('[%s] Node-0 seems not available. Check the next.' % self.label)
+				n_nodes = len(self.server_list.keys())
 					## More than 1 nodes prepared
-					if n_nodes > 1:
-						## --- Node selection
-						for i in self.server_list.keys():
-							if i == '0':
-								continue
-							la_n = self.getLatency(self.server_list[i]['host'], self.check_scheme, timeout=self.server_list[i]['timeout']/1000.)
-							if la_n >= 0:
-								self.log.print('Select node-%s, with latency of %.2f ms.' % (i, la_n * 1000.))
-								if nodeSelected != i:
-									self.log.print('Switch to node-%s from node-%s.' % (i, nodeSelected), False)
-									isNodeSwitching = True
-								else:
-									self.log.print('Keep node-%s.' % i)
-								nodeSelected = i
-								isNodeSelected = True
-								break
-							else:
-								self.log.print('Node-%s is also failed. Next...' % i)
-						## --- End node selection
-
-						## All nodes died
-						if isNodeSelected == False:
-							self.log.print('Seems no node is available... What about node-0 now?')
-							time.sleep(self.check_interval)
+				if n_nodes > 1:
+					## --- Node selection
+					for i in self.server_list.keys():
+						if i == '0':
 							continue
-				## --- End node failover logic
-
-				## When node-0 is working / can work normally
+						la_n = self.getLatency(self.server_list[i]['host'], self.check_scheme, timeout=self.server_list[i]['timeout']/1000.)
+						if la_n >= 0:
+							self.log.print('[%s] Select node-%s, with latency of %.2f ms.' % (self.label, i, la_n * 1000.))
+							if self.nodeSelected != i:
+								self.log.print('[%s] Switch to node-%s from node-%s.' % (self.label, i, self.nodeSelected), False)
+								self.isNodeSwitching = True
+							else:
+								self.log.print('[%s] Keep node-%s.' % (self.label, i))
+							self.nodeSelected = i
+							self.isNodeSelected = True
+							break
+						else:
+							self.log.print('[%s] Node-%s is also failed. Next...' % (self.label, i))
+					## --- End node selection
+					## All nodes died
+					if self.isNodeSelected == False:
+						self.log.print('[%s] Seems no node is available... What about node-0 now?' % self.label)
+						return -1
+			## --- End node failover logic
+			## When node-0 is working / can work normally
+			else:
+				self.log.print('[%s] Node-0 is available, with latency of %.2f ms.' % (self.label, la_0 * 1000.))
+				self.isNodeSelected = True
+				if self.nodeSelected != "0":
+					self.log.print('[%s] Switch to node-0 from node-%s.' % (self.label, self.nodeSelected), 1)
+					self.isNodeSwitching = True
 				else:
-					self.log.print('Node-0 is available, with latency of %.2f ms.' % (la_0 * 1000.))
-					isNodeSelected = True
-					if nodeSelected != "0":
-						self.log.print('Switch to node-0 from node-%s.' % (nodeSelected), 1)
-						isNodeSwitching = True
-					else:
-						self.log.print('Keep node-0.')
-					nodeSelected = "0"
+					self.log.print('[%s] Keep node-0.' % self.label)
+				self.nodeSelected = "0"
 
-				## When node switching is needed (or GLOHA is the 1st time launched)
-				if isNodeSwitching:
-					self.log.print('Executing task of node-%s...' % nodeSelected)
-					## When old process is running, kill it
-					if taskproc != None:
-						self.log.print('Kill the old process.')
-						os.system('kill -9 %d' % self.pid)
-						taskproc.kill()
-						self.log.print('Exec> %s' % ' '.join(self.server_list[nodeSelected]['exec']))
+			## When node switching is needed (or GLOHA is the 1st time launched)
+			if self.isNodeSwitching:
+				self.log.print('[%s] Executing task of node-%s...' % (self.label, self.nodeSelected))
+				## When old process is running, kill it
+				if self.taskproc != None:
+					self.log.print('[%s] Kill the old process.' % self.label)
+					os.system('kill -9 %d' % self.pid)
+					self.taskproc.kill()
+					self.log.print('[%s] Exec> %s' % (self.label, ' '.join(self.server_list[self.nodeSelected]['exec'])))
+				## Run a new process
+				self.taskproc = subprocess.Popen(self.server_list[self.nodeSelected]['exec'], shell=True)
+				self.pid = self.taskproc.pid
+				# self._wait4Lock()
+				ajs.gracefulEditJSON(PATH_PID, {self.label: self.pid})
+				# lock_0 = False
+				self.log.print('[%s] PID: %d' % (self.label, self.pid))
+				self.isNodeSwitching = False
+			else:
+				self.log.print('[%s] Node is not switched.' % self.label)
 
-					## Run a new process
-					taskproc = subprocess.Popen(self.server_list[nodeSelected]['exec'], shell=True)
-					self.pid = taskproc.pid
-					self._wait4Lock()
-					ajs.gracefulEditJSON(PATH_PID, {self.label: self.pid})
-					lock_0 = False
-					self.log.print('PID: %d' % self.pid)
-					isNodeSwitching = False
-				else:
-					self.log.print('Node is not switched.')
-
-				time.sleep(self.check_interval)
 
 class GHA:
 	def __init__(self, config_fname, log_level=0):
@@ -153,12 +151,12 @@ class GHA:
 		self.config_fname = config_fname
 		self.config = ajs.gracefulLoadJSON(config_fname)
 		self.taskQueen = []
-		self.taskmap = {}
+		self.timetable = None
+
+		self.isConfigBackuped = False
 
 	def __del__(self):
-		for t in self.taskQueen:
-			self.taskmap[t.getLabel()].terminate()
-			system('kill -9 %d' % pid_map[t.getLabel()])
+		self.terminateRunning()
 
 	def _isUpdatedConfigExist(self):
 		try:
@@ -190,69 +188,76 @@ class GHA:
 			return -1
 		return container
 
-	def _startConfigMonitor(self, interval=10):
-		isConfigBackuped = False
-		while True:
-			time.sleep(interval)
-			self.log.print('<Monitor>Checking if there is update in config file.')
+	def _startConfigMonitor(self):
+		
+		self.log.print('<Monitor>Checking if there is update in config file.')
 
-			## Error detected in loading config file
-			if self._isUpdatedConfigExist() < 0:
+		## Error detected in loading config file
+		if self._isUpdatedConfigExist() < 0:
+			self.log.print('<Monitor>Config error detected. Use previous config.')
+
+			## Save the latest correct config
+			if self.isConfigBackuped == False:
+				ajs.gracefulDumpJSON(self.config_fname + '.bak', self.config)
+				self.isConfigBackuped = True
+				## <Wait for the next period>
+
+		## Update detected in config file
+		elif self._isUpdatedConfigExist() == True:
+			self.log.print('<Monitor>Update detected in config file.', write=False)
+			upacked_new_config = self._configUnpack()
+			## Error detected in apply config file
+			if upacked_new_config == -1:
 				self.log.print('<Monitor>Config error detected. Use previous config.')
 
 				## Save the latest correct config
-				if isConfigBackuped == False:
+				if self.isConfigBackuped == False:
 					ajs.gracefulDumpJSON(self.config_fname + '.bak', self.config)
-					isConfigBackuped = True
-
-			## Update detected in config file
-			elif self._isUpdatedConfigExist() == True:
-				self.log.print('<Monitor>Update detected in config file.', write=False)
-				upacked_new_config = self._configUnpack()
-				## Error detected in apply config file
-				if upacked_new_config == -1:
-					self.log.print('<Monitor>Config error detected. Use previous config.')
-
-					## Save the latest correct config
-					if isConfigBackuped == False:
-						ajs.gracefulDumpJSON(self.config_fname + '.bak', self.config)
-						isConfigBackuped = True
+					self.isConfigBackuped = True
+					## <Wait for the next period>
 				
-				## No problem in updated config file
-				else:
-					isConfigBackuped = False
-					self.reloadConfig()
-
-			## No update in config file
+			## No problem in updated config file
 			else:
-				self.log.print('<Monitor>No update in config file.')
-				isConfigBackuped = False
+				self.isConfigBackuped = False
+				self.reloadConfig()
+				## <Wait for the next period>
+
+		## No update in config file
+		else:
+			self.log.print('<Monitor>No update in config file.')
+			isConfigBackuped = False
+			## <Wait for the next period>
 
 	def startDaemon(self):
 		aut.gracefulWrite(PATH_PID, '{}')
 		self.taskQueen = self._configUnpack()
 		if self.taskQueen == -1:
 			return -1
+		self.timetable = scheduler.Scheduler()
+		self.log.print('<Daemon>Adding task...', 0)
 		for t in self.taskQueen:
-			self.taskmap[t.getLabel()] = multiprocessing.Process(target=t.exec)
-			self.taskmap[t.getLabel()].start()
-		self._startConfigMonitor()
+			self.timetable.addTask([t.getInterval(), t.exec, None])
+		self.timetable.addTask([10, self._startConfigMonitor, None])
+		self.log.print('<Daemon>Start periodic tasks.', 0)
+		self.timetable.periodicExecute(atStart=True, acc=1.)
 
 	def terminateRunning(self):
 		pid_map = ajs.gracefulLoadJSON(PATH_PID)
-		for t in self.taskQueen:
-			self.log.print('<Monitor>Kill PID %d' % pid_map[t.getLabel()])
-			self.taskmap[t.getLabel()].terminate()
-			system('kill -9 %d' % pid_map[t.getLabel()])
+		for t in pid_map.keys():
+			self.log.print('<Monitor>Kill PID %d' % pid_map[t])
+			os.system('kill -9 %d' % pid_map[t])
+		self.timetable.terminate()
 
 	def reloadConfig(self):
 		self.log.print('Reload config...')
 		self.terminateRunning()
 		self.config = ajs.gracefulLoadJSON(self.config_fname)
 		self.taskQueen = self._configUnpack()
+		self.timetable = scheduler.Scheduler()
 		for t in self.taskQueen:
-			self.taskmap[t.getLabel()] = multiprocessing.Process(target=t.exec)
-			self.taskmap[t.getLabel()].start()
+			self.timetable.addTask([t.getInterval(), t.exec, None])
+		self.timetable.addTask([10, self._startConfigMonitor, None])
+		self.timetable.periodicExecute(atStart=True, acc=1.)
 
 if __name__ == '__main__':
 	path_conf = sys.argv[1]
@@ -260,7 +265,7 @@ if __name__ == '__main__':
 		pid_map = ajs.gracefulLoadJSON(PATH_PID)
 		for p in pid_map.keys():
 			print('Kill PID %d...' % pid_map[p])
-			system('kill -9 %d' % pid_map[p])
+			os.system('kill -9 %d' % pid_map[p])
 	else:
 		log_level = int(sys.argv[2])
 		g = GHA(path_conf, log_level)
--- scheduler.py
+++ scheduler.py
@@ -1,26 +1,42 @@
 import time
 
-class multiTimer:
+class Scheduler:
 	def __init__(self):
 		self.task_pool = []  ## [period, func, args]
+		self.stop_signal = False
 	
 	def addTask(self, task):
 		self.task_pool.append(task)
+
+	def terminate(self):
+		self.stop_signal = True
 	
-	def periodicExecute(self, acc=1.):
+	def periodicExecute(self, atStart=False, acc=1.):
+		if atStart:
+			for t in self.task_pool:
+				if t[2] == None:
+					t[1]()
+				else:
+					t[1](t[2])
+
 		t0 = time.time()
 		task_pool_with_clock = self.task_pool
 		for t in task_pool_with_clock:
 			t.append(t0)
-		print(task_pool_with_clock)
 		## task_pool_with_clock: [period, func, args, clock]
+
 		while True:
-			time.sleep(acc)
+			if self.stop_signal:
+				break
 			for t in task_pool_with_clock:
 				t_now = time.time()
 				if t_now - t[3] >= t[0]:
 					t[3] = t_now
-					t[1](t[2])
+					if t[2] == None:
+						flag_turbo = t[1]()
+					else:
+						flag_turbo = t[1](t[2])
+			time.sleep(acc)
 			
 if __name__ == '__main__':
 	def func(n):
