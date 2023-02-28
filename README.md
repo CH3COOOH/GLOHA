657e070219c4a6a11aefb837e6ab7ef7d2170c04
asuka <asuka>
2023-02-28 21:32:33 +0900

    Defunct issue fixed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GLOHA
 GLOHA (Global High Availability), run `gloha <config_filename.json> <-t|0~2>`.
+
+# Bug-230228
+Process out of control when a session is empty (all nodes died) and then the config is reloaded.
--- gloha.py
+++ gloha.py
@@ -4,7 +4,6 @@ import subprocess
 import time
 import os
 
-import azlib.ut as aut
 import azlib.pr as apr
 import azlib.json as ajs
 import tcp_latency
@@ -64,6 +63,13 @@ class GHA_INSTANCE:
 	def getLabel(self):
 		return self.label
 
+	def suicide(self):
+		## This structure can prevent <defunct>
+		## It has been known on 2023.02.28
+		self.taskproc.send_signal(2)
+		self.taskproc.wait()
+		self.taskproc.poll()
+
 	def exec(self):
 		if self.select_scheme == 'priority':
 			## Until 2023.02.25, "priority" is the only scheme of GLOHA
@@ -74,18 +80,22 @@ class GHA_INSTANCE:
 			## When node-0 failed, select an available one.
 			## >>--- Node failover logic
 			if la_0 < 0:
+				## Means that the node-0 died
 				self.log.print('[%s] Node-0 seems not available. Check the next.' % self.label)
 				n_nodes = len(self.server_list.keys())
-					## More than 1 nodes prepared
+				
 				if n_nodes > 1:
+					## More than 1 nodes prepared
 					## >>--- Node selection
 					for i in self.server_list.keys():
 						if i == '0':
 							continue
 						la_n = self.getLatency(self.server_list[i]['host'], self.check_scheme, timeout=self.server_list[i]['timeout']/1000.)
 						if la_n >= 0:
+							## Means the node is available, and will be selected
 							self.log.print('[%s] Select node-%s, with latency of %.2f ms.' % (self.label, i, la_n * 1000.))
 							if self.nodeSelected != i:
+								## Means that the previous node died, and this is a new one
 								self.log.print('[%s] Switch to node-%s from node-%s.' % (self.label, i, self.nodeSelected), False)
 								self.isNodeSwitching = True
 							else:
@@ -94,8 +104,10 @@ class GHA_INSTANCE:
 							self.isNodeSelected = True
 							break
 						else:
+							## Means this node is also died
 							self.log.print('[%s] Node-%s is also failed. Next...' % (self.label, i))
 					## <<--- End node selection
+
 					## All nodes died
 					if self.isNodeSelected == False:
 						self.log.print('[%s] Seems no node is available... What about node-0 now?' % self.label)
@@ -105,21 +117,20 @@ class GHA_INSTANCE:
 			else:
 				self.log.print('[%s] Node-0 is available, with latency of %.2f ms.' % (self.label, la_0 * 1000.))
 				self.isNodeSelected = True
-				if self.nodeSelected != "0":
+				if self.nodeSelected != '0':
 					self.log.print('[%s] Switch to node-0 from node-%s.' % (self.label, self.nodeSelected), 1)
 					self.isNodeSwitching = True
 				else:
 					self.log.print('[%s] Keep node-0.' % self.label)
-				self.nodeSelected = "0"
+				self.nodeSelected = '0'
 
 			## When node switching is needed (or GLOHA is the 1st time launched)
 			if self.isNodeSwitching:
-				self.log.print('[%s] Executing task of node-%s...' % (self.label, self.nodeSelected))
+				self.log.print('[%s] Executing task of new node-%s...' % (self.label, self.nodeSelected))
 				## When old process is running, kill it
 				if self.taskproc != None:
 					self.log.print('[%s] Kill the old process.' % self.label)
-					os.system('kill -9 %d' % self.pid)
-					self.taskproc.kill()
+					self.suicide()
 					self.log.print('[%s] Exec> %s' % (self.label, ' '.join(self.server_list[self.nodeSelected]['exec'])))
 				## Run a new process
 				self.taskproc = subprocess.Popen(self.server_list[self.nodeSelected]['exec'], shell=True)
@@ -135,7 +146,6 @@ class GHA:
 	def __init__(self, config_fname, log_level=0):
 		self.log = apr.Log(log_level)
 		self.log_level = log_level
-		self.isLocked = False
 		self.config_fname = config_fname
 		self.config = ajs.gracefulLoadJSON(config_fname)
 		self.taskQueen = []
@@ -154,23 +164,21 @@ class GHA:
 			self.log.print('<Update>Syntax error detected from the config file.', 2)
 			return -1
 
-	def _configUnpack(self):
+	def _configUnpack(self, ext_conf=None, isCheckMode=False):
 		container = []
-		# for profile in self.config.keys():
-		# 		label = profile
-		# 		check_interval = self.config[profile]['check_interval']
-		# 		check_scheme = self.config[profile]['check_scheme']
-		# 		select_scheme = self.config[profile]['select_scheme']
-		# 		server_list = self.config[profile]['server_list']
-		# 		container.append(GHA_INSTANCE(label, check_interval, check_scheme, select_scheme, server_list, self.log_level))
+		if ext_conf == None:
+			config = self.config
+		else:
+			config = ext_conf
 		try:
-			for profile in self.config.keys():
+			for profile in config.keys():
 				label = profile
-				check_interval = self.config[profile]['check_interval']
-				check_scheme = self.config[profile]['check_scheme']
-				select_scheme = self.config[profile]['select_scheme']
-				server_list = self.config[profile]['server_list']
-				container.append(GHA_INSTANCE(label, check_interval, check_scheme, select_scheme, server_list, self.log_level))
+				check_interval = config[profile]['check_interval']
+				check_scheme = config[profile]['check_scheme']
+				select_scheme = config[profile]['select_scheme']
+				server_list = config[profile]['server_list']
+				if isCheckMode == False:
+					container.append(GHA_INSTANCE(label, check_interval, check_scheme, select_scheme, server_list, self.log_level))
 		except:
 			self.log.print('<Unpack>Syntax error detected from the config file.', 2)
 			return -1
@@ -179,73 +187,75 @@ class GHA:
 	def _startConfigMonitor(self):
 		
 		self.log.print('<Monitor>Checking if there is update in config file.')
+		isUpdated = self._isUpdatedConfigExist()
 
-		## Error detected in loading config file
-		if self._isUpdatedConfigExist() < 0:
+		if isUpdated < 0:
+			## Error detected in loading config file
 			self.log.print('<Monitor>Config error detected. Use previous config.', 2)
 
 			## Save the latest correct config
 			if self.isConfigBackuped == False:
 				ajs.gracefulDumpJSON(self.config_fname + '.bak', self.config)
 				self.isConfigBackuped = True
-				## <Wait for the next period>
+			return -1
+			## <Wait for the next period>
 
-		## Update detected in config file
-		elif self._isUpdatedConfigExist() == True:
-			self.log.print('<Monitor>Update detected in config file.', 1)
-			upacked_new_config = self._configUnpack()
-			## Error detected in apply config file
-			if upacked_new_config == -1:
+		elif isUpdated == True:
+			## Update detected in config file
+			self.log.print('<Monitor>Update detected in config file.')
+			if self._configUnpack(ext_conf=ajs.gracefulLoadJSON(self.config_fname), isCheckMode=True) == -1:
+				## Error detected in apply config file
 				self.log.print('<Monitor>Config error detected. Use previous config.', 2)
 
 				## Save the latest correct config
 				if self.isConfigBackuped == False:
+					## When the config is not backuped
 					ajs.gracefulDumpJSON(self.config_fname + '.bak', self.config)
 					self.isConfigBackuped = True
-					## <Wait for the next period>
-				
-			## No problem in updated config file
-			else:
-				self.isConfigBackuped = False
-				self.reloadConfig()
+				return -1
 				## <Wait for the next period>
-
-		## No update in config file
+			else:
+				## No problem in updated config file
+				self.isConfigBackuped = False
+				self.reloadConfig(False)
+				return 0
+				## <Wait for the next period>
 		else:
+			## No update in config file
 			self.log.print('<Monitor>No update in config file.')
 			isConfigBackuped = False
+			return 0
 			## <Wait for the next period>
 
-	def startDaemon(self):
-		aut.gracefulWrite(PATH_PID, '{}')
+	def terminateRunning(self):
+		pid_map = ajs.gracefulLoadJSON(PATH_PID)
+		# for t in pid_map.keys():
+		# 	self.log.print('<Monitor>Kill PID %d' % pid_map[t])
+		# 	os.system('kill -9 %d' % pid_map[t])
+		for t in self.taskQueen:
+			t.suicide()
+		self.timetable.terminate()
+
+	def reloadConfig(self, isFirstTime):
+		if isFirstTime == False:
+			self.log.print('<Reload>Reload config...')
+			self.terminateRunning()
+			self.config = ajs.gracefulLoadJSON(self.config_fname)
 		self.taskQueen = self._configUnpack()
 		if self.taskQueen == -1:
 			return -1
 		self.timetable = scheduler.Scheduler()
-		self.log.print('<Daemon>Adding task...', 0)
+		self.log.print('<Reload>Adding task...', 0)
 		for t in self.taskQueen:
 			self.timetable.addTask([t.getInterval(), t.exec, None])
 		self.timetable.addTask([10, self._startConfigMonitor, None])
-		self.log.print('<Daemon>Start periodic tasks.', 0)
+		self.log.print('<Reload>Start periodic tasks.', 0)
 		self.timetable.periodicExecute(atStart=True, acc=PERODIC_ACC)
 
-	def terminateRunning(self):
-		pid_map = ajs.gracefulLoadJSON(PATH_PID)
-		for t in pid_map.keys():
-			self.log.print('<Monitor>Kill PID %d' % pid_map[t])
-			os.system('kill -9 %d' % pid_map[t])
-		self.timetable.terminate()
+	def startDaemon(self):
+		ajs.gracefulDumpJSON(PATH_PID, {})
+		self.reloadConfig(True)
 
-	def reloadConfig(self):
-		self.log.print('Reload config...')
-		self.terminateRunning()
-		self.config = ajs.gracefulLoadJSON(self.config_fname)
-		self.taskQueen = self._configUnpack()
-		self.timetable = scheduler.Scheduler()
-		for t in self.taskQueen:
-			self.timetable.addTask([t.getInterval(), t.exec, None])
-		self.timetable.addTask([10, self._startConfigMonitor, None])
-		self.timetable.periodicExecute(atStart=True, acc=PERODIC_ACC)
 
 def ps():
 	pid_map = ajs.gracefulLoadJSON(PATH_PID)
